7cfed7cb4570501a5a6cb0df74685ff4a8484c81
Josh Steiner <josh@jsteiner.me>
2016-03-11 15:32:56 -0500

    Match response from HTTP.Client

--- lib/cog_api/fake/authentication.ex
+++ lib/cog_api/fake/authentication.ex
@@ -1,5 +1,5 @@
 defmodule CogApi.Fake.Authentication do
   alias CogApi.Endpoint
 
-  def get_and_merge_token(%Endpoint{}=endpoint), do: endpoint
+  def get_and_merge_token(%Endpoint{}=endpoint), do: {:ok, endpoint}
 end
