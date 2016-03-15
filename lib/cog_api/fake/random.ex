122fd9770276133bdbbd514b32d5ae81c0656e18
Josh Steiner <josh@jsteiner.me>
2016-03-15 14:15:27 -0400

    Seed random so we get different results
    Method stolen from: http://neo.com/2014/02/24/pseudo-random-number-generation-in-elixir/

--- lib/cog_api/fake/random.ex
+++ lib/cog_api/fake/random.ex
@@ -1,8 +1,20 @@
 defmodule CogApi.Fake.Random do
+  @on_load :seed_random
+
   def random_string(digits) do
     random_number
     |> to_string
     |> String.slice(2, digits)
+  end
+
+  def seed_random do
+    :random.seed({
+      :crypto.rand_uniform(1, 99999),
+      :crypto.rand_uniform(1, 99999),
+      :crypto.rand_uniform(1, 99999)
+    })
+
+    :ok
   end
 
   defp random_number do
