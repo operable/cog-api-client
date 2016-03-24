5a72135a93ec7976d5c84db8aec3d23843b10e3a
Josh Steiner <josh@jsteiner.me>
2016-03-24 16:24:12 -0400

    Allow forcing an auth failure for Fake client

--- lib/cog_api/fake/authentication.ex
+++ lib/cog_api/fake/authentication.ex
@@ -1,7 +1,11 @@
 defmodule CogApi.Fake.Authentication do
   alias CogApi.Endpoint
 
-  def get_and_merge_token(%Endpoint{token: nil}=endpoint) do
-    {:ok, %{endpoint | token: "1234"}}
+  def get_and_merge_token(%Endpoint{token: nil, password: password}=endpoint) do
+    if password == "INVALID_PASSWORD" do
+      {:error, ["Invalid username/password"]}
+    else
+      {:ok, %{endpoint | token: "1234"}}
+    end
   end
 end
---
+++ test/unit/cog_api/fake/authentication_test.exs
@@ -0,0 +1,36 @@
+defmodule CogApi.Fake.AuthenticationTest do
+  use CogApi.FakeCase
+  doctest CogApi.Fake.Authentication
+
+  alias CogApi.Fake.Authentication
+
+  describe "authenticate" do
+    context "when the username and password are valid" do
+      it "returns an endpoint with the token attached" do
+        endpoint = %Endpoint{
+          username: "admin",
+          password: "password",
+          host: "localhost",
+          port: "4000",
+        }
+        {:ok, updated_endpoint} = Authentication.get_and_merge_token(endpoint)
+
+        assert present updated_endpoint.token
+      end
+    end
+
+    context "when the username or password is invalid" do
+      it "returns a descriptive error" do
+        endpoint = %Endpoint{
+          username: "username",
+          password: "INVALID_PASSWORD",
+          host: "localhost",
+          port: "4000",
+        }
+        {:error, errors} = Authentication.get_and_merge_token(endpoint)
+
+        assert errors == ["Invalid username/password"]
+      end
+    end
+  end
+end
