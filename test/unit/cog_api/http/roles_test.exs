f92479680bb5b619f758fe23142d016b8b53b509
Josh Steiner <josh@jsteiner.me>
2016-03-31 15:53:40 -0400

    Clean up roles tests

--- test/unit/cog_api/http/roles_test.exs
+++ test/unit/cog_api/http/roles_test.exs
@@ -8,11 +8,15 @@ defmodule CogApi.HTTP.RolesTest do
   describe "role_index" do
     it "returns a list of roles" do
       cassette "roles_index" do
-        {:ok, roles} = Client.role_index(valid_endpoint)
+        endpoint = valid_endpoint
+        params = %{name: "role_index"}
+        {:ok, role} = Client.role_create(endpoint, params)
 
-        first_role = List.first roles
-        assert present first_role.id
-        assert present first_role.name
+        {:ok, roles} = Client.role_index(endpoint)
+
+        last_role = List.last(roles)
+        assert last_role.id == role.id
+        assert last_role.name == role.name
       end
     end
   end
@@ -22,7 +26,7 @@ defmodule CogApi.HTTP.RolesTest do
       it "returns the role" do
         cassette "roles_show" do
           endpoint = valid_endpoint
-          params = %{name: "QA Analyst"}
+          params = %{name: "role_show"}
           {:ok, created_role} = Client.role_create(endpoint, params)
 
           {:ok, found_role} = Client.role_show(endpoint, created_role.id)
@@ -36,11 +40,11 @@ defmodule CogApi.HTTP.RolesTest do
   describe "role_create" do
     it "returns the created role" do
       cassette "roles_create" do
-        params = %{"name" => "new role"}
+        params = %{"name" => "role_create"}
         {:ok, role} = Client.role_create(valid_endpoint, params)
 
         assert present role.id
-        assert role.name == "new role"
+        assert role.name == params["name"]
       end
     end
   end
@@ -49,7 +53,7 @@ defmodule CogApi.HTTP.RolesTest do
     it "returns the updated role" do
       cassette "role_update" do
         endpoint = valid_endpoint
-        {:ok, new_role} = Client.role_create(endpoint, %{name: "new role"})
+        {:ok, new_role} = Client.role_create(endpoint, %{name: "role_update"})
         {:ok, updated_role} = Client.role_update(
           endpoint,
           new_role.id,
@@ -65,7 +69,7 @@ defmodule CogApi.HTTP.RolesTest do
     it "returns :ok" do
       cassette "role_delete" do
         endpoint = valid_endpoint
-        {:ok, role} = Client.role_create(endpoint, %{name: "role to delete"})
+        {:ok, role} = Client.role_create(endpoint, %{name: "role_delete"})
         assert :ok == Client.role_delete(endpoint, role.id)
       end
     end
@@ -75,8 +79,8 @@ defmodule CogApi.HTTP.RolesTest do
     it "returns the roles that are associated with that group" do
       cassette "role_grant" do
         endpoint = valid_endpoint
-        role = Client.role_create(endpoint, %{name: "role"}) |> get_value
-        group = Client.group_create(endpoint, %{name: "group"}) |> get_value
+        role = Client.role_create(endpoint, %{name: "role_grant"}) |> get_value
+        group = Client.group_create(endpoint, %{name: "group_role_grant"}) |> get_value
 
         updated_group = Client.role_grant(endpoint, role, group) |> get_value
 
@@ -90,8 +94,8 @@ defmodule CogApi.HTTP.RolesTest do
     it "returns the roles that are associated with that group" do
       cassette "role_revoke" do
         endpoint = valid_endpoint
-        role = Client.role_create(endpoint, %{name: "role123"}) |> get_value
-        group = Client.group_create(endpoint, %{name: "group123"}) |> get_value
+        role = Client.role_create(endpoint, %{name: "role_revoke"}) |> get_value
+        group = Client.group_create(endpoint, %{name: "group_role_revoke"}) |> get_value
         group = Client.role_grant(endpoint, role, group) |> get_value
         assert group.roles != []
 
