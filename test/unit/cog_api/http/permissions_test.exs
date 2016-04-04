0607bb8c079fc1a2f375861fd8829f55bf23b017
Josh Steiner <josh@jsteiner.me>
2016-04-04 10:54:45 -0400

    Clean up permissions tests

--- test/unit/cog_api/http/permissions_test.exs
+++ test/unit/cog_api/http/permissions_test.exs
@@ -11,7 +11,6 @@ defmodule CogApi.HTTP.PermissionsTest do
         {:ok, permissions} = Client.permission_index(valid_endpoint)
 
         first_permission = List.first permissions
-
         assert present first_permission.id
         assert present first_permission.name
         assert present first_permission.namespace
@@ -22,11 +21,11 @@ defmodule CogApi.HTTP.PermissionsTest do
   describe "permission_create" do
     it "returns the created permission, in the site namespace" do
       cassette "permission_create" do
-        name = "foobar"
+        name = "permission_create"
         {:ok, permission} = Client.permission_create(valid_endpoint, name)
 
         assert present permission.id
-        assert permission.name == "foobar"
+        assert permission.name == name
         assert permission.namespace == "site"
       end
     end
