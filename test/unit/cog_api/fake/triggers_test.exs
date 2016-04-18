a742d1d23de25c3296099b2fd0a973ac5f8d0742
Christopher Maier <chris@operable.io>
2016-04-18 12:59:29 -0400

    Expose trigger enabled state

--- test/unit/cog_api/fake/triggers_test.exs
+++ test/unit/cog_api/fake/triggers_test.exs
@@ -47,6 +47,7 @@ defmodule CogApi.Fake.TriggersTest do
       assert present(trigger.id)
       assert trigger.name == params.name
       assert trigger.pipeline == params.pipeline
+      assert trigger.enabled == true
       assert trigger.as_user == params.as_user
       assert trigger.timeout_sec == params.timeout_sec
       assert trigger.description == params.description
