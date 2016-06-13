409adcf3729b33d84cd8bd49bcafd9f890435fb8
vanstee <patrick@vanstee.me>
2016-06-13 09:24:13 -0600

    Deserialize command rules into struct

--- lib/cog_api/resources/command.ex
+++ lib/cog_api/resources/command.ex
@@ -1,12 +1,14 @@
 defmodule CogApi.Resources.Command do
   @derive [Poison.Encoder]
 
+  alias CogApi.Resources.Rule
+
   defstruct [
     :id,
     :description,
     :documentation,
     :name,
     :bundle,
-    rules: [],
+    rules: [%Rule{}],
   ]
 end
