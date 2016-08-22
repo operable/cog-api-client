2c9c44fe27defedb3670ae36952535382e0253a8
Kevin Smith <kevin@operable.io>
2016-08-22 08:54:12 -0400

    Bump version for next release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CogApi.Mixfile do
   def project do
     [
       app: :cog_api,
-      version: "0.13.0",
+      version: "0.14.0",
       elixir: "~> 1.3.1",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
