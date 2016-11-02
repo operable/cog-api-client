ac0570e2d31945b295ba9733da6b772591146da5
Operable Engineering <engineering@operable.io>
2016-11-02 11:18:04 -0400

    Updated app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CogApi.Mixfile do
   def project do
     [
       app: :cog_api,
-      version: "0.16.0",
+      version: "0.17.0",
       elixir: "~> 1.3.1",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
